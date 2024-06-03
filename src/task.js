b7647d896b8894dbb24f4f906be6d27eb21a3161
Jose Cruz <resoj.cruz@gmail.com>
2024-06-03 07:03:14 -0400

    Add complete parameter to task object

--- src/task.js
+++ src/task.js
@@ -1,8 +1,9 @@
 export class Task {
-    constructor(taskTitle, description, dueDate, priority) {
+    constructor(taskTitle, description, dueDate, priority, complete=false) {
         this.taskTitle = taskTitle;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.complete = complete;
     }
 }
